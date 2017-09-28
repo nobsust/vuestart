ebe29c32e0ee86ed63881b9817c8ecea00bf39f7
Samadhi Jayasinghe <sjy@sl.tiqri.com>
2017-09-28 09:30:01 +0530

    nested components

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-Vue.component('task', {
+Vue.component('task-list', {
 
-	template: '<li>1000</li>'
+	template: '<div><task v-for="task in tasks">{{task.description}}</task></div>',
+
+	data() {
+
+		return {
+			tasks: [
+				{ description: 'Task One', completed: true },
+				{ description: 'Task Two', completed: false },
+				{ description: 'Task Three', completed: false },
+				{ description: 'Task Four', completed: true },
+				{ description: 'Task Five', completed: false }
+			]
+		}
+
+	}
 
 })
 
-// template: '<li><slot></slot></li>'
+Vue.component('task', {
+
+	template: '<li><slot></slot></li>'
+
+})
 
 new Vue({
 
